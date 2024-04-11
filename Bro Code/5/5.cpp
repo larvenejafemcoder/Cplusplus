ec3e6237558a8bedca0763702e9672ed54d2bf4c
itzaRinna <dutchismahdick@gmail.com>
2024-04-11 17:50:39 +0700

    5

--- Bro Code/5/5.cpp
+++ Bro Code/5/5.cpp
@@ -4,7 +4,15 @@ using std::cout;
 using std::endl;
 using std::cin;
 
+typedef std::std::vector<char> v;
+
 int main()
 {
-	
+	// typedef = reserved keyword used to create an additional name
+	// 			 (alias) for another data type.
+	// 			 New indentifier for an existing type
+	//			 Helps with readability and reduces typos
+
+	return 0;
+
 }
