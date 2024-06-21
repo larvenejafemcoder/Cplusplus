45aa5b713bafd01d81dc2146b4d941c5183fd27f
itzaRinna <dutchismahdick@gmail.com>
2024-06-21 07:41:25 +0700

    K Team Ep 2-3

---
+++ K Team/2/2/2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello HowKteam.com" << endl;
+    system("pause");
+    return 0;
+}
---
+++ K Team/2/2/Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int test()
+{
+    cout << "Hello HowKteam.com" << endl;
+    system("pause");
+    return 0;
+}
