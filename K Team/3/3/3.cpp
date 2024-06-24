e0f85ef7d8c787edd0aacd3543a742a6f73381b0
itzaRinna <dutchismahdick@gmail.com>
2024-06-24 14:17:48 +0700

    3

---
+++ K Team/3/3/3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int nSoNguyen;
+
+    nSoNguyen = 69;
+    
+    nSoNguyen = (6 + 9) * (6 - 9);
+
+    cout << "Hello HowKTeam.com!\n";
+    return 0;
+}
+
+// Các câu l?nh và bi?u th?c
+// Hàm
+// Th? vi?n
